406cdfbd2b54aad3f02bc1565fde4a4f5d5bacdb
George Burton <burtonageo@gmail.com>
2016-09-07 22:18:50 +0100

    Make iokit #[no_std]

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![allow(non_camel_case_types,non_upper_case_globals,non_snake_case)]
+#![no_std]
 
-extern crate core;
 extern crate CoreFoundation_sys as cf;
 extern crate libc;
 extern crate mach;
